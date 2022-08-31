4d723eaa99b5eaa5bb14d4d43703c85e3984e040
Julien Fraichot <julien.fraichot@hyland.com>
2022-08-31 17:04:56 +0200

    issuer to send credential

--- issuer/src/index.ts
+++ issuer/src/index.ts
@@ -1,11 +1,22 @@
 import certificate from '../data/testnet-v3-did.json';
+import { loadPolyfillOnce } from '../../wallet/src/helpers/loadPolyfillOnce';
+// @ts-ignore
+import { WebCredential } from 'credential-handler-polyfill';
+
+async function sendCredential (): Promise<void> {
+  const webCredential = new WebCredential(
+    'VerifiableCredential',
+    certificate
+  );
+  const result = await navigator.credentials.store(webCredential);
+  console.log(result);
+}
 
 function init (): void {
+  loadPolyfillOnce();
   document
     .getElementsByClassName('js-get-certificate')[0]
-    .addEventListener('click', function () {
-      console.log('get the certificate', certificate);
-    });
+    .addEventListener('click', sendCredential);
 }
 
 init();
