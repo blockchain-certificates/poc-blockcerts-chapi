c01428b8c82438e4f31560b213b409c49ea6eda3
Julien Fraichot <julien.fraichot@hyland.com>
2022-09-15 16:02:43 +0200

    add option to store cert to local device

--- rollup.iife.wallet.worker.store.config.js
+++ rollup.iife.wallet.worker.store.config.js
@@ -1,17 +1,15 @@
 import typescript from '@rollup/plugin-typescript';
-import json from '@rollup/plugin-json';
 
 export default {
   input: 'wallet/src/worker-store.ts',
   output: [
     {
-      file: 'wallet/lib/worker-store.js',
-      format: 'iife',
+      dir: 'wallet/lib/',
+      format: 'esm',
       name: 'ChapiWalletWorkerStore'
     }
   ],
   plugins: [
-    typescript(),
-    json()
+    typescript()
   ]
 };
--- wallet/src/worker-store.ts
+++ wallet/src/worker-store.ts
@@ -3,9 +3,10 @@ import * as WebCredentialHandler from 'web-credential-handler';
 // @ts-ignore
 import * as CredentialHandlerPolyfill from 'credential-handler-polyfill';
 import { EventHandlerResultType } from './models/EventHandler';
+import * as NativeFileSystemAdapter from 'native-file-system-adapter';
 import type { EventHandlerResponseType } from './models/EventHandler';
 
-async function storeData (credentialHandlerEvent): void {
+async function storeData (credentialHandlerEvent): Promise<void> {
   const certificate = credentialHandlerEvent.credential;
   const result = await fetch('http://localhost:4555/store', {
     method: 'POST',
@@ -16,6 +17,7 @@ async function storeData (credentialHandlerEvent): void {
   }).then(response => response.json());
 
   if (result.status === 200) {
+    // @ts-ignore
     credentialHandlerEvent.respondWith<EventHandlerResponseType>({
       type: EventHandlerResultType.Response,
       dataType: 'VerifiableCredential',
@@ -26,12 +28,38 @@ async function storeData (credentialHandlerEvent): void {
   }
 }
 
+async function storeToDevice(credentialHandlerEvent): Promise<void> {
+  const certificate = credentialHandlerEvent.credential.data;
+  const fileHandle = await NativeFileSystemAdapter.showSaveFilePicker({
+    _preferPolyfill: false,
+    suggestedName: `${certificate.credentialSubject?.claim?.name ?? certificate.id}.json`,
+    types: [
+      { accept: { 'text/json': [ 'json' ] } }
+    ],
+    excludeAcceptAllOption: false // default
+  });
+
+  const writer = await fileHandle.createWritable();
+  await writer.write(JSON.stringify(certificate));
+  await writer.close();
+  console.log('closing');
+  // @ts-ignore
+  credentialHandlerEvent.respondWith<EventHandlerResponseType>({
+    type: EventHandlerResultType.Response,
+    dataType: 'VerifiableCredential',
+    data: JSON.stringify(certificate)
+  });
+}
+
 async function handleStoreEvent () {
   const credentialHandlerEvent = await WebCredentialHandler.receiveCredentialEvent();
   console.log('Wallet processing store() event:', credentialHandlerEvent);
   document
-    .getElementsByClassName('js-store-ok')[0]
+    .getElementsByClassName('js-store-server')[0]
     .addEventListener('click', storeData.bind(null, credentialHandlerEvent));
+  document
+    .getElementsByClassName('js-store-device')[0]
+    .addEventListener('click', storeToDevice.bind(null, credentialHandlerEvent));
 }
 
 async function init (): Promise<void> {
